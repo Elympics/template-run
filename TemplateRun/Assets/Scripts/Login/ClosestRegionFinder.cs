1899e311d60a0c7c81a10b6dd5ce6e16c25495f7
Patryk Chojnicki <patryk.chojnicki@daftmobile.com>
2023-09-18 10:52:32 +0000

    Bump SDK version from 0.7.2 to 0.9.1, enable all regions, remove ElympicsLobbyClient prefab from the gameplay scene and adjust README file.

---
+++ TemplateRun/Assets/Scripts/Login/ClosestRegionFinder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using Elympics;
+using Cysharp.Threading.Tasks;
+
+public static class ClosestRegionFinder
+{
+    private static (string Region, float LatencyMs)? CachedClosestRegion;
+
+    public static async UniTask<(string Region, float LatencyMs)> GetClosestRegion()
+    {
+        if (!CachedClosestRegion.HasValue)
+        {
+            Debug.Log("Searching for closest region...");
+            CachedClosestRegion = await ElympicsCloudPing.ChooseClosestRegion(ElympicsRegions.AllAvailableRegions);
+            Debug.Log("Closest region has been cached!");
+        }
+
+        return CachedClosestRegion.Value;
+    }
+}
